57fb8716673a8edfcf750e11120c86a9ecb8f210
Merouane Atig <merouane.atig@gmail.com>
2014-05-22 23:02:15 +0200

    display correctly ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
 
   def self.all_ratings
-    Movie.select(:rating).distinct
+    Movie.select(:rating).uniq.map { |m| m.rating }
   end
 end
